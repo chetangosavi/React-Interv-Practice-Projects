f0eeb90453484c43b4d906e524d1a99b7a9920a9
Chetan Gosavi <chetangosavi81@gmail.com>
2025-04-29 17:08:53 +0530

    added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,41 @@
-import TransferList from "./component/TransferList"
-
-import CouterTimer from "./component/CouterTimer"
-import ProgressBar from "./component/ProgressBar"
-import PBarPage from "./pages/PBarPage"
-import AccordionComponent from "./component/AccordionComponent"
-import AcordionComp from "./component/AcoComponent/AcordionComp"
+import TransferList from "./component/TransferList";
+import CouterTimer from "./component/CouterTimer";
+import ProgressBar from "./component/ProgressBar";
+import PBarPage from "./pages/PBarPage";
+import AccordionComponent from "./component/AccordionComponent";
+import AcordionComp from "./component/AcoComponent/AcordionComp";
 
 const App = () => {
-
-  const arr = [1,2,3,4,5]
+  const arr = [1, 2, 3, 4, 5];
   return (
     <div className=" mt-10">
-      <CouterTimer/>
-      <TransferList/>
-      <ProgressBar/>
-      <PBarPage/>
 
-{/* Accordion Component */}
-      <div className="mt-10">
-        <div className="text-center text-2xl font-bold">Accordion Component</div>
-      {
-        arr.map(value=> <AccordionComponent key={value}/>)
-      }
-      </div>
+      {/* Counter Timer Compoent */}
+      <CouterTimer />
 
+      {/* TransferList Component */}
+      <TransferList />
 
-{/* Accordion Component - Using json file */}
-    <div className="mt-20 mb-20">
-     <AcordionComp/> 
+      {/* ProgressBar Components */}
+      <ProgressBar />
+      <PBarPage />
+
+      {/* Accordion Component */}
+        <div className="mt-10">
+          <div className="text-center text-2xl font-bold">
+            Accordion Component
+          </div>
+          {arr.map((value) => (
+            <AccordionComponent key={value} />
+          ))}
+        </div>
+
+      {/* Accordion Component - Using json file */}
+        <div className="mt-20 mb-20">
+          <AcordionComp />
+        </div>
     </div>
-        
-      
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
